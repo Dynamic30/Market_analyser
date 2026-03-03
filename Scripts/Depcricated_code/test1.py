1b1157689466c46d0c0e41b21f801c42baffd218
Dynamic30 <aryanwork3012@gmail.com>
2026-03-03 07:50:35 +0000

    minor changes

--- Scripts/Depcricated_code/test1.py
+++ Scripts/Depcricated_code/test1.py
@@ -1,10 +1,11 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import pandas_ta as ta # type: ignore
+import pandas_ta as ta 
 import pathlib
 import json
 from datetime import date
+
 ticker = yf.Ticker(f"{"RELIANCE"}.NS")
 print(ticker)
 info = ticker.info
@@ -12,3 +13,7 @@ info = ticker.info
 df = ticker.history(period="1mo")
 
 print(df)
+
+df.ta.atr(length=14,append=True)
+
+print(df['ATRr_14'])
